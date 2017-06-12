816d504c4ef64c41ac75a8146bb8364a97d82747
jiehome <passionjie2014@163.com>
2017-06-12 23:31:59 +0800

    all

--- app/src/main/java/com/mfitandroid7/MainActivity.java
+++ app/src/main/java/com/mfitandroid7/MainActivity.java
@@ -1,8 +1,16 @@
 package com.mfitandroid7;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.libraryforfileprovider.FileProvider7;
 
@@ -10,13 +18,19 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView mIvPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mIvPhoto = (ImageView) findViewById(R.id.id_iv);
+        // 6.0原生系统自行打开SDCard权限!
     }
 
     public void installApk(View v){
@@ -39,8 +53,40 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-
+    private static final int REQUEST_CODE_TAKE_PHOTO = 0x110;
+    private String mCurrentPhotoPath;
     public void shareFile(View v){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            String filename = new SimpleDateFormat("yyyyMMdd-HHmmss", Locale.CHINA)
+                    .format(new Date()) + ".png";
+            File file = new File(getExternalCacheDir(), filename);
+            mCurrentPhotoPath = file.getAbsolutePath();
+
+            Uri fileUri = FileProvider7.getUriForFile(this, file);
+
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+            startActivityForResult(takePictureIntent, REQUEST_CODE_TAKE_PHOTO);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
+        if (bitmap != null) {
+            mIvPhoto.setImageBitmap(bitmap);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.i("==", "onActivityResult: ");
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_TAKE_PHOTO) {
+            mIvPhoto.setImageBitmap(BitmapFactory.decodeFile(mCurrentPhotoPath));
+        }
+        // else tip?
 
     }
 }
